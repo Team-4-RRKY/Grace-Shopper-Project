59b46de6f7c383ef592c12cf1b80c60e7b9cd116
Kathleen Lopez <kat.ari.lopez@gmail.com>
2018-06-06 16:32:09 -0400

    refactored userRoutes

--- server/api/users.js
+++ server/api/users.js
@@ -5,22 +5,21 @@ module.exports = router;
 
 //don't forget to HIDE password
 router.get( '/', defaultHandler(async (req, res, next) => {
-    await User.findAll({
-      include: [{ model: Watch }]
-    })
-      .then(users => res.json(users))
-      .catch(next);
-  }));
+  const allUsers = await User.findAll({
+    include: [{ model: Watch }]
+  })
+  res.json(allUsers)
+}));
 
 router.get( '/:id', defaultHandler(async (req, res, next) => {
-    const userId = req.params.id;
+  const userId = req.params.id;
 
-    const userInstance = await User.findById(userId, {
-      include: [{ model: Watch }]
-    });
+  const userInstance = await User.findById(userId, {
+    include: [{ model: Watch }]
+  });
 
-    res.json(userInstance);
-  }));
+  res.json(userInstance);
+}));
 
 router.post( '/', defaultHandler( async (req, res, next) => {
   const newUserInstance = await User.create(req.body)
@@ -28,10 +27,16 @@ router.post( '/', defaultHandler( async (req, res, next) => {
 }));
 
 router.put('/:id', defaultHandler( async (req, res, next) => {
-  await User.update(req.body, {where: {
+  //Can I use null in place of numOfUsers
+  const [ numOfUsers, updatedUsers ] = await User.update(req.body, {
+    where: {
     id: req.params.id
-  }})
-    res.send({message: 'User Instance Updated'})
+  },
+  returning: true,
+  plain: true
+})
+
+    res.send({ message: 'User Instance Updated', updatedUsers[0] })
 }))
 
 router.delete('/:id', defaultHandler( async (req, res, next) => {
