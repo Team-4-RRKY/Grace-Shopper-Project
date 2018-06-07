91071edae992cbcb8145a9a238a221951a9c3310
Kathleen Lopez <kat.ari.lopez@gmail.com>
2018-06-07 17:56:41 -0400

    VIEW:ABOUTUS :: compelted componenet w/ minimal styling

--- client/components/AboutUs.js
+++ client/components/AboutUs.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import { Typography, CardContent, CardHeader, Card} from '@material-ui/core';
 
-const AboutUs = () => (
-  <div>
-    <h2>About bayWatch</h2>
-    <p>
-    Could someone survive inside a transporter buffer for 75 years? When has justice ever been as simple as a rule book? Talk about going nowhere fast. My oath is between Captain Kargan and myself. Your only concern is with how you obey my orders. Or do you prefer the rank of prisoner to that of lieutenant? The unexpected is our normal routine.
-    </p>
-    <p>
-    Why don't we just give everybody a promotion and call it a night - 'Commander'? Commander William Riker of the Starship Enterprise. Fate protects fools, little children and ships named Enterprise. When has justice ever been as simple as a rule book? Mr. Crusher, ready a collision course with the Borg ship. You enjoyed that. But the probability of making a six is no greater than that of rolling a seven. That might've been one of the shortest assignments in the history of Starfleet. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion?
-    </p>
-    <p>
-    I guess it's better to be lucky than good. Is it my imagination, or have tempers become a little frayed on the ship lately? This is not about revenge. This is about justice. The Enterprise computer system is controlled by three primary main processor cores, cross-linked with a redundant melacortz ramistat, fourteen kiloquad interface modules. The Federation's gone; the Borg is everywhere! Not if I weaken first. A lot of things can change in twelve years, Admiral. Now, how the hell do we defeat an enemy that knows us better than we know ourselves?
-    </p>
-  </div>
-)
+const AboutUs = (props) => {
+
+return (
+  <Card className="card pos .card" classes="card-title-style" >
+    <CardHeader className="card-title" title="BayWatch" />
+    <CardContent>
+      <Typography component="p">
+      Could someone survive inside a transporter buffer for 75 years? When has justice ever been as simple as a rule book? Talk about going nowhere fast. My oath is between Captain Kargan and myself. Your only concern is with how you obey my orders. Or do you prefer the rank of prisoner to that of lieutenant? The unexpected is our normal routine.
+      </Typography>
+      <Typography component="p">
+      Why don't we just give everybody a promotion and call it a night - 'Commander'? Commander William Riker of the Starship Enterprise. Fate protects fools, little children and ships named Enterprise. When has justice ever been as simple as a rule book? Mr. Crusher, ready a collision course with the Borg ship. You enjoyed that. But the probability of making a six is no greater than that of rolling a seven. That might've been one of the shortest assignments in the history of Starfleet. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion?
+      </Typography>
+      <Typography component="p">
+      I guess it's better to be lucky than good. Is it my imagination, or have tempers become a little frayed on the ship lately? This is not about revenge. This is about justice. The Enterprise computer system is controlled by three primary main processor cores, cross-linked with a redundant melacortz ramistat, fourteen kiloquad interface modules. The Federation's gone; the Borg is everywhere! Not if I weaken first. A lot of things can change in twelve years, Admiral. Now, how the hell do we defeat an enemy that knows us better than we know ourselves?
+      </Typography>
+      <Typography component="p">
+      We know you're dealing in stolen ore. But I wanna talk about the assassination attempt on Lieutenant Worf. The game's not big enough unless it scares you a little. Now, how the hell do we defeat an enemy that knows us better than we know ourselves? Wait a minute - you've been declared dead. You can't give orders around here. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. Worf, It's better than music. It's jazz. I can't. As much as I care about you, my first duty is to the ship. We finished our first sensor sweep of the neutral zone. Fate protects fools, little children and ships named Enterprise. This is not about revenge. This is about justice. Yes, absolutely, I do indeed concur, wholeheartedly! Mr. Worf, you sound like a man who's asking his friend if he can start dating his sister.
+      </Typography>
+      <Typography component="p">
+      Smooth as an android's bottom, eh, Data? Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. How long can two people talk about nothing? You enjoyed that. In all trust, there is the possibility for betrayal. I recommend you don't fire until you're within 40,000 kilometers. Then maybe you should consider this: if anything happens to them, Starfleet is going to want a full investigation. A surprise party? Mr. Worf, I hate surprise parties. I would *never* do that to you.
+      </Typography>
+    </CardContent>
+  </Card>
+)}
 
 export default AboutUs;
