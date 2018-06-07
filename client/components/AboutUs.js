905d7666b453bd83c40c7a963b7903545396da65
Kathleen Lopez <kat.ari.lopez@gmail.com>
2018-06-07 12:41:55 -0400

    VIEW:ABOUTUS :: added About Us file and dumb component

---
+++ client/components/AboutUs.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const AboutUs = () => (
+  <div>
+    <h2>About bayWatch</h2>
+    <p>
+    Could someone survive inside a transporter buffer for 75 years? When has justice ever been as simple as a rule book? Talk about going nowhere fast. My oath is between Captain Kargan and myself. Your only concern is with how you obey my orders. Or do you prefer the rank of prisoner to that of lieutenant? The unexpected is our normal routine.
+    </p>
+    <p>
+    Why don't we just give everybody a promotion and call it a night - 'Commander'? Commander William Riker of the Starship Enterprise. Fate protects fools, little children and ships named Enterprise. When has justice ever been as simple as a rule book? Mr. Crusher, ready a collision course with the Borg ship. You enjoyed that. But the probability of making a six is no greater than that of rolling a seven. That might've been one of the shortest assignments in the history of Starfleet. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion?
+    </p>
+    <p>
+    I guess it's better to be lucky than good. Is it my imagination, or have tempers become a little frayed on the ship lately? This is not about revenge. This is about justice. The Enterprise computer system is controlled by three primary main processor cores, cross-linked with a redundant melacortz ramistat, fourteen kiloquad interface modules. The Federation's gone; the Borg is everywhere! Not if I weaken first. A lot of things can change in twelve years, Admiral. Now, how the hell do we defeat an enemy that knows us better than we know ourselves?
+    </p>
+  </div>
+)
+
+export default AboutUs;
