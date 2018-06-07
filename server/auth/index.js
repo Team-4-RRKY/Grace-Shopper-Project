2e4f924e6810a21b03e7df3409692b5e1ea37191
Raghava Kamalesh <raghava.kamalesh@gmail.com>
2018-06-07 18:05:04 -0400

    added cart actions and thunk to redux functionality

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,45 +1,64 @@
-const router = require('express').Router()
-const User = require('../db/models/user')
-module.exports = router
+const router = require('express').Router();
+const { User, Watch } = require('../db/models');
+module.exports = router;
 
 router.post('/login', (req, res, next) => {
-  User.findOne({where: {email: req.body.email}})
+  console.log('login running');
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+    include: [
+      {
+        model: Watch,
+        as: 'saleItems',
+      },
+      {
+        model: Watch,
+        as: 'cartItems',
+      },
+      {
+        model: Watch,
+        as: 'listings',
+      },
+    ],
+  })
     .then(user => {
       if (!user) {
-        console.log('No such user found:', req.body.email)
-        res.status(401).send('Wrong username and/or password')
+        console.log('No such user found:', req.body.email);
+        res.status(401).send('Wrong username and/or password');
       } else if (!user.correctPassword(req.body.password)) {
-        console.log('Incorrect password for user:', req.body.email)
-        res.status(401).send('Wrong username and/or password')
+        console.log('Incorrect password for user:', req.body.email);
+        res.status(401).send('Wrong username and/or password');
       } else {
-        req.login(user, err => (err ? next(err) : res.json(user)))
+        req.login(user, err => (err ? next(err) : res.json(user)));
       }
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
 router.post('/signup', (req, res, next) => {
   User.create(req.body)
     .then(user => {
-      req.login(user, err => (err ? next(err) : res.json(user)))
+      req.login(user, err => (err ? next(err) : res.json(user)));
     })
     .catch(err => {
       if (err.name === 'SequelizeUniqueConstraintError') {
-        res.status(401).send('User already exists')
+        res.status(401).send('User already exists');
       } else {
-        next(err)
+        next(err);
       }
-    })
-})
+    });
+});
 
 router.post('/logout', (req, res) => {
-  req.logout()
-  req.session.destroy()
-  res.redirect('/')
-})
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+});
 
 router.get('/me', (req, res) => {
-  res.json(req.user)
-})
+  res.json(req.user);
+});
 
-router.use('/google', require('./google'))
+router.use('/google', require('./google'));
