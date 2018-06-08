3984458c0df3b629ed2d798372844ab7bdc3034b
Kathleen Lopez <kat.ari.lopez@gmail.com>
2018-06-08 14:20:40 -0400

    added carousel component  file

---
+++ client/components/carousel.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Carousel } from 'react-boostrap';
+
+class WatchCarousel extends Component{
+  render(){
+    return(
+      <Carousel></Carousel>
+    )
+  }
+}
