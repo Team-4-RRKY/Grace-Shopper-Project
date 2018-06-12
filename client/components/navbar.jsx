aa607fd8f6a8fbf347ba1eb27980b2aa0c9a3190
archon21 <rwetmore3@gmail.com>
2018-06-12 15:35:04 -0400

    Undid all changes from merge

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,68 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
+import { Menu, Button, MenuItem } from '@material-ui/core';
 
-const Navbar = ({ handleClick, isLoggedIn, cart, guestCart }) => (
-  <div>
-    <h1 id="title">
-      <Link to="/home">BayWatch</Link>
-    </h1>
-    <nav>
-      {isLoggedIn ? (
-        <div className="nav-container">
-          {/* The navbar will show these links after you log in */}
-          <Link className="nav-item" to="/home">
-            Home
-          </Link>
-          <Link className="nav-item" to="/watches/browse">
-            Watches
-          </Link>
-          <Link className="nav-item" to="/user">
-            Account
-          </Link>
-          <Link className="nav-item" to="/aboutus">
-            About Us
-          </Link>
-          <a className="nav-item" href="#" onClick={handleClick}>
-            Logout
-          </a>
-          <Link className="nav-item" to="/cart">
-            Cart{' '}
-            {` (${cart.reduce((acc, el) => {
-              return acc + el.cart.quantity;
-            }, 0)})`}
-          </Link>
-          <Link className="nav-item" to="/sell"> Sell Watch</Link>
-        </div>
-      ) : (
-        <div className="nav-container">
-          {/* The navbar will show these links before you log in */}
-          <Link className="nav-item" to="/login">
-            Login
-          </Link>
-          <Link className="nav-item" to="/signup">
-            Sign Up
-          </Link>
-          <Link className="nav-item" to="/watches/browse">
-            Watches
-          </Link>
-          <Link className="nav-item" to="/aboutus">
-            About Us
-          </Link>
-          <Link className="nav-item" to="/cart">
-            Cart{' '}
-            {` (${guestCart.reduce((acc, el) => {
-              return acc + el.cart.quantity;
-            }, 0)})`}
-          </Link>
-        </div>
-      )}
-    </nav>
-    <div className="divider" />
-  </div>
-);
+class Navbar extends Component {
+  state = {
+    anchorEl: null,
+    open: false
+  };
+
+  handleOpen = (event) => {
+    this.setState({ open: true, anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false, anchorEl: null });
+  };
+
+  render() {
+    let { handleClick, isLoggedIn, cart, guestCart, user } = this.props;
+    return (
+      <div>
+        <h1 id="title">BayWatch</h1>
+        <nav>
+          {isLoggedIn ? (
+            <div className="nav-container">
+              {/* The navbar will show these links after you log in */}
+              <Link className="nav-item" to="/home">
+                Home
+              </Link>
+              <Link className="nav-item" to="/watches/browse">
+                Watches
+              </Link>
+              <Button onClick={this.handleOpen}>
+              <div className="nav-item">
+                {user.firstName + ' ' + user.lastName}
+              </div>
+              </Button>
+              <Menu anchorEl={this.state.anchorEl} open={this.state.open} onClose={this.handleClose}>
+                <MenuItem onClick={this.handleClose}>
+                  <Link className="nav-item" to="/user">
+                    {user.firstName + "'s" + ' Account'}
+                  </Link>
+                </MenuItem>
+                <MenuItem onClick={this.handleClose}>
+                  <Link className="nav-item" to="/sell">
+                    Sell Watch
+                  </Link>
+                </MenuItem>
+                <MenuItem href="#" onClick={handleClick}>
+                  <div className="nav-item"> Logout</div>
+                </MenuItem>
+              </Menu>
+              <Link className="nav-item" to="/cart">
+                Cart{' '}
+                {` (${cart.reduce((acc, el) => {
+                  return acc + el.cart.quantity;
+                }, 0)})`}
+              </Link>
+            </div>
+          ) : (
+            <div className="nav-container">
+              {/* The navbar will show these links before you log in */}
+              <Link className="nav-item" to="/login">
+                Login
+              </Link>
+              <Link className="nav-item" to="/signup">
+                Sign Up
+              </Link>
+              <Link className="nav-item" to="/watches/browse">
+                WATCHES
+              </Link>
+              <Link className="nav-item" to="/cart">
+                Cart{' '}
+                {` (${guestCart.reduce((acc, el) => {
+                  return acc + el.cart.quantity;
+                }, 0)})`}
+              </Link>
+            </div>
+          )}
+        </nav>
+        <div className="divider" />
+      </div>
+    );
+  }
+}
 
 /**
  * CONTAINER
@@ -72,6 +96,7 @@ const mapState = state => {
     isLoggedIn: !!state.user.user.id,
     cart: state.user.user.cartItems,
     guestCart: state.user.guestCart,
+    user: state.user.user
   };
 };
 
@@ -79,7 +104,7 @@ const mapDispatch = dispatch => {
   return {
     handleClick() {
       dispatch(logout());
-    },
+    }
   };
 };
 
@@ -93,5 +118,5 @@ export default connect(
  */
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired
 };
--- client/components/user-components/user.jsx
+++ client/components/user-components/user.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom'
-import { Button, Dialog, DialogTitle, DialogContent, GridList, GridListTile, GridListTileBar } from '@material-ui/core';
-import UserForm  from './userForm.jsx'
-import {editUserData} from '../../store/user'
+import { Link } from 'react-router-dom';
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  GridList,
+  GridListTile,
+  GridListTileBar
+} from '@material-ui/core';
+import UserForm from './userForm.jsx';
+import { editUserData } from '../../store/user';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -31,82 +39,87 @@ class User extends Component {
   handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
-    })
-  }
+    });
+  };
 
-  handleSubmit = (evt) => {
-    this.props.submitEdit(this.state)
-  }
+  handleSubmit = () => {
+    this.props.submitEdit(this.state);
+  };
 
   render() {
     const { user } = this.props;
     return (
-      <div className="user-car-wrapper">
-      <Card className="user-card">
-        <CardMedia
-          className="user-media"
-          image={user.image}
-          title={user.firstName}
-        />
-          <CardContent className="user-card-content">
-            <Typography gutterBottom variant="headline" component="h2">
-              {user.firstName + ' ' + user.lastName}
-            </Typography>
-            <Typography component="p">
-              User: {user.firstName} {user.lastName}
-            </Typography>
-            <Typography component="p">
-              Email: {user.email}
-            </Typography>
-            <Typography component="p">
-              Address: {user.address}
-            </Typography>
-            <Typography component="p">
-              Gender: {user.gender}
-            </Typography>
-            <Typography component="p">
-              Joined On: {user.createdAt.slice(0, 10)}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button onClick={this.handleOpen} size="small" color="primary">
-              Edit User
-            </Button>
-          </CardActions>
-          <Dialog
-            open={this.state.open}
-            onClose={this.handleClose}
-           >
-           <DialogTitle>Edit {user.firstName}'s Account</DialogTitle>
-           <DialogContent>
-             <UserForm displayName="Submit Edit" handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
-           </DialogContent>
-          </Dialog>
-      </Card>
-      <GridList>
-        {user.listings.map(listing => (
-          <GridListTile key={listing.id}>
-            <Link to = {`/watches/${listing.id}`}>
-              <img src={listing.image} />
-              <GridListTileBar title={listing.brand} subtitle={listing.model} />
-            </Link>
-          </GridListTile>
-        ))}
-      </GridList>
+      <div className="column wrap">
+      <div className="row wrap">
+        <div>
+          <Card >
+            <CardMedia
+              className="user-media"
+              image={user.image}
+              title={user.firstName}
+            />
+            <CardContent className="user-card-content">
+              <Typography gutterBottom variant="headline" component="h2">
+                {user.firstName + ' ' + user.lastName}
+              </Typography>
+              <Typography component="p">
+                User: {user.firstName} {user.lastName}
+              </Typography>
+              <Typography component="p">Email: {user.email}</Typography>
+              <Typography component="p">Address: {user.address}</Typography>
+              <Typography component="p">Gender: {user.gender}</Typography>
+              <Typography component="p">
+                Joined On: {user.createdAt.slice(0, 10)}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button onClick={this.handleOpen} size="small" color="primary">
+                Edit User
+              </Button>
+            </CardActions>
+            <Dialog open={this.state.open} onClose={this.handleClose}>
+              <DialogTitle>Edit {user.firstName}'s Account</DialogTitle>
+              <DialogContent>
+                <UserForm
+                  displayName="Submit Edit"
+                  handleChange={this.handleChange}
+                  handleSubmit={this.handleSubmit}
+                />
+              </DialogContent>
+            </Dialog>
+          </Card>
+        </div>
+      </div>
+        <div>
+        <h2>{'Watches ' + user.firstName + ' is Selling.'}</h2>
+          <GridList cols={4}>
+            {user.listings.map(listing => (
+              <GridListTile key={listing.id}>
+                <Link to={`/watches/${listing.id}`}>
+                  <img src={listing.image} />
+                  <GridListTileBar
+                    title={listing.brand}
+                    subtitle={listing.model}
+                  />
+                </Link>
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  user: state.user.user,
+  user: state.user.user
 });
 
 const mapDispatchToProps = dispatch => ({
   submitEdit: editData => {
-    dispatch(editUserData(editData))
+    dispatch(editUserData(editData));
   }
-})
+});
 
 export default connect(
   mapStateToProps,
