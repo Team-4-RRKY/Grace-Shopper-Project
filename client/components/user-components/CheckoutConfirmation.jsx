24d25809cc11fb64b2fc4fe1bb7fbd3370becc0a
Kathleen Lopez <kat.ari.lopez@gmail.com>
2018-06-12 14:24:17 -0400

    checkoutconfirmation component styled

---
+++ client/components/user-components/CheckoutConfirmation.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing.unit * 3,
+  }),
+});
+
+const watches = [
+  {
+    id: 1,
+    brand: 'Audemars Piguet',
+    model: '40 Series - 40MM',
+    image: 'http://www.pkicon.com/icons/31929/Clock-256.png',
+    price: '$120',
+    quantity: 15,
+    style: 'Classic',
+    tier: 'Luxurious',
+    gender: 'Gentleman',
+    sellerId: 17,
+  },
+  {
+    id: 2,
+    brand: 'Audemars Piguet',
+    model: '40 Series - 40MM',
+    image: 'http://www.pkicon.com/icons/31929/Clock-256.png',
+    price: '$120',
+    quantity: 15,
+    style: 'Classic',
+    tier: 'Luxurious',
+    gender: 'Gentleman',
+    sellerId: 17,
+  },
+  {
+    id: 3,
+    brand: 'Audemars Piguet',
+    model: '40 Series - 40MM',
+    image: 'http://www.pkicon.com/icons/31929/Clock-256.png',
+    price: '$120',
+    quantity: 15,
+    style: 'Classic',
+    tier: 'Luxurious',
+    gender: 'Gentleman',
+    sellerId: 17,
+  }
+]
+
+function CheckoutConfirmation(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <Paper className={classes.root} elevation={4}>
+        <Typography
+          variant="display3"
+          align="center">
+          ..:: Order Confirmation::..
+        </Typography>
+        <div className="checkout-watch-div">
+          {watches.map( watch => {
+            return (
+            <Typography
+              key={watch.id}
+              variant="headline"
+              align="right">
+              {watch.brand} -- {watch.model} || {watch.price}
+            </Typography>
+          )})}
+        </div>
+        <div className="checkout-total-div">
+          <Typography
+            variant="title"
+            align="right">Order Total: $TOTAL
+          </Typography>
+        </div>
+      </Paper>
+    </div>
+  );
+}
+
+CheckoutConfirmation.propTypes = {
+  classes: PropTypes.object.isRequired,
+  // purchasedItems: PropTypes.array,
+};
+
+export default withStyles(styles)(CheckoutConfirmation);
